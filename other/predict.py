aeb7092041ed1e0cffaa9c4549b8f03f22d216f0
Sydres <sungshingman@yahoo.com.hk>
2020-12-31 13:45:48 +0800

    train,predict and function modify

--- other/predict.py
+++ other/predict.py
@@ -2,16 +2,39 @@ import tensorflow as tf
 import cv2
 import numpy as np
 
+Label = ['Black footed Albatross', 'Laysan Albatross', 'Sooty Albatross', 'Groove billed Ani', 'Crested Auklet', 'Least Auklet', 'Parakeet Auklet', 'Rhinoceros Auklet', 'Brewer Blackbird', 'Red winged Blackbird', 'Rusty Blackbird', 'Yellow headed Blackbird', 'Bobolink', 'Indigo Bunting', 'Lazuli Bunting', 'Painted Bunting', 'Cardinal', 'Spotted Catbird', 'Gray Catbird', 'Yellow breasted Chat', 'Eastern Towhee', 'Chuck will Widow', 'Brandt Cormorant', 'Red faced Cormorant', 'Pelagic Cormorant', 'Bronzed Cowbird', 'Shiny Cowbird', 'Brown Creeper', 'American Crow', 'Fish Crow', 'Black billed Cuckoo', 'Mangrove Cuckoo', 'Yellow billed Cuckoo', 'Gray crowned Rosy Finch', 'Purple Finch', 'Northern Flicker', 'Acadian Flycatcher', 'Great Crested Flycatcher', 'Least Flycatcher', 'Olive sided Flycatcher', 'Scissor tailed Flycatcher', 'Vermilion Flycatcher', 'Yellow bellied Flycatcher', 'Frigatebird', 'Northern Fulmar', 'Gadwall', 'American Goldfinch', 'European Goldfinch', 'Boat tailed Grackle', 'Eared Grebe', 'Horned Grebe', 'Pied billed Grebe', 'Western Grebe', 'Blue Grosbeak', 'Evening Grosbeak', 'Pine Grosbeak', 'Rose breasted Grosbeak', 'Pigeon Guillemot', 'California Gull', 'Glaucous winged Gull', 'Heermann Gull', 'Herring Gull', 'Ivory Gull', 'Ring billed Gull', 'Slaty backed Gull', 'Western Gull', 'Anna Hummingbird', 'Ruby throated Hummingbird', 'Rufous Hummingbird', 'Green Violetear', 'Long tailed Jaeger', 'Pomarine Jaeger', 'Blue Jay', 'Florida Jay', 'Green Jay', 'Dark eyed Junco', 'Tropical Kingbird', 'Gray Kingbird', 'Belted Kingfisher', 'Green Kingfisher', 'Pied Kingfisher', 'Ringed Kingfisher', 'White breasted Kingfisher', 'Red legged Kittiwake', 'Horned Lark', 'Pacific Loon', 'Mallard', 'Western Meadowlark', 'Hooded Merganser', 'Red breasted Merganser', 'Mockingbird', 'Nighthawk', 'Clark Nutcracker', 'White breasted Nuthatch', 'Baltimore Oriole', 'Hooded Oriole', 'Orchard Oriole', 'Scott Oriole', 'Ovenbird', 'Brown Pelican', 'White Pelican', 'Western Wood Pewee', 'Sayornis', 'American Pipit', 'Whip poor Will', 'Horned Puffin', 'Common Raven', 'White necked Raven', 'American Redstart', 'Geococcyx', 'Loggerhead Shrike', 'Great Grey Shrike', 'Baird Sparrow', 'Black throated Sparrow', 'Brewer Sparrow', 'Chipping Sparrow', 'Clay colored Sparrow', 'House Sparrow', 'Field Sparrow', 'Fox Sparrow', 'Grasshopper Sparrow', 'Harris Sparrow', 'Henslow Sparrow', 'Le Conte Sparrow', 'Lincoln Sparrow', 'Nelson Sharp tailed Sparrow', 'Savannah Sparrow', 'Seaside Sparrow', 'Song Sparrow', 'Tree Sparrow', 'Vesper Sparrow', 'White crowned Sparrow', 'White throated Sparrow', 'Cape Glossy Starling', 'Bank Swallow', 'Barn Swallow', 'Cliff Swallow', 'Tree Swallow', 'Scarlet Tanager', 'Summer Tanager', 'Artic Tern', 'Black Tern', 'Caspian Tern', 'Common Tern', 'Elegant Tern', 'Forsters Tern', 'Least Tern', 'Green tailed Towhee', 'Brown Thrasher', 'Sage Thrasher', 'Black capped Vireo', 'Blue headed Vireo', 'Philadelphia Vireo', 'Red eyed Vireo', 'Warbling Vireo', 'White eyed Vireo', 'Yellow throated Vireo', 'Bay breasted Warbler', 'Black and white Warbler', 'Black throated Blue Warbler', 'Blue winged Warbler', 'Canada Warbler', 'Cape May Warbler', 'Cerulean Warbler', 'Chestnut sided Warbler', 'Golden winged Warbler', 'Hooded Warbler', 'Kentucky Warbler', 'Magnolia Warbler', 'Mourning Warbler', 'Myrtle Warbler', 'Nashville Warbler', 'Orange crowned Warbler', 'Palm Warbler', 'Pine Warbler', 'Prairie Warbler', 'Prothonotary Warbler', 'Swainson Warbler', 'Tennessee Warbler', 'Wilson Warbler', 'Worm eating Warbler', 'Yellow Warbler', 'Northern Waterthrush', 'Louisiana Waterthrush', 'Bohemian Waxwing', 'Cedar Waxwing', 'American Three toed Woodpecker', 'Pileated Woodpecker', 'Red bellied Woodpecker', 'Red cockaded Woodpecker', 'Red headed Woodpecker', 'Downy Woodpecker', 'Bewick Wren', 'Cactus Wren', 'Carolina Wren', 'House Wren', 'Marsh Wren', 'Rock Wren', 'Winter Wren', 'Common Yellowthroat']
 
-model = tf.keras.models.load_model("test.h5")
+model = tf.keras.models.load_model("./model/0.6819.h5")
 
-image = cv2.imread("Yellow_Warbler_1.jpg")
-image = cv2.resize(image,(200,200))
+image = cv2.imread("a.jpg")
+image = cv2.resize(image,(224,224))
 image = np.expand_dims(image, axis=0)
 image = image/255.0
 image = np.asarray(image)
-print(image.shape)
-print(image)
-print(type(image))
+# print(image.shape)
+# print(image)
+# print(type(image))
 
-print(model.predict_classes(image))
+ravel_predict = model.predict(image).ravel()
+# print(model.predict(image))
+i = []
+b = []
+for index , bird in enumerate(ravel_predict):
+    i.append(index)
+    b.append(bird)
+predict_result = list(zip(i,b))
+# print(predict_result)
+
+res = sorted(predict_result, key = lambda x: x[1], reverse = True)[:5]
+Top5_similar_bird = []
+for x in range(len(res)):
+
+    for y in range(len(Label)):
+        if y == res[x][0]:
+            Top5_similar_bird.append(Label[y])
+            break
+print("1st:",Top5_similar_bird[0])
+print("2nd:",Top5_similar_bird[1])
+print("3rd:",Top5_similar_bird[2])
+print("4th:",Top5_similar_bird[3])
+print("5th:",Top5_similar_bird[4])
--- other/train.py
+++ other/train.py
@@ -1,89 +1,54 @@
-from function import load_data_name
-import tensorflow as tf
-import numpy as np
-import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
-from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import  Flatten, Dense, Dropout, Conv2D, MaxPooling2D, ZeroPadding2D , Activation ,AveragePooling2D ,AveragePooling1D , BatchNormalization ,GlobalAveragePooling2D , GlobalMaxPooling2D
+from tensorflow.keras.optimizers import SGD , Adam
+from tensorflow.keras.callbacks import EarlyStopping , ModelCheckpoint
+from tensorflow.keras import regularizers
+import tensorflow as tf
+import cv2, numpy as np
 import joblib
 
-# (x,y) = load_data()
-
-# x_train, x_test, y_train,y_test = train_test_split(x,y,test_size=0.1)
-x_train = joblib.load("x.pkl")
-y_train = joblib.load("y.pkl")
+x_train = joblib.load("./dataset/x_200_224.pkl")
+y_train = joblib.load("./dataset/y_200_224.pkl")
 
 x_train = x_train/255.0
-
-x_train = np.asarray(x_train)
-y_train = np.asarray(y_train)
-
-# print("x shape",x_train.shape)
-# print("y shape",y_train.shape)
-
-# x_train, x_test, y_train,y_test = train_test_split(x_train,y_train,test_size=0.1)
+vgg = tf.keras.applications.vgg16.VGG16(input_shape=x_train.shape[1:],include_top=False,weights='imagenet')
 
 model = Sequential()
-model.add( Conv2D(32,(3,3),input_shape = x_train.shape[1:]) )
-model.add(Activation("relu") )
-model.add(MaxPooling2D(pool_size=(3,3)))
+# print(vgg.layers[0])
+
+i = 0
+for layer in vgg.layers:
+    i+=1
+    if i > 15 :
+        break
+    model.add(layer)
+
+# model.add(Conv2D(1000,1 ))
+# model.add(Activation("relu"))
+# model.add(AveragePooling2D((1,1)))
+
+model.add(Conv2D(2000,1))
+model.add(MaxPooling2D((2,2),strides=(2,2)))
 
 
-model.add( Conv2D(64,(3,3)) )
-model.add(Activation("relu") )
-model.add( Conv2D(128,(3,3)) )
-model.add(Activation("relu") )
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Conv2D(1000,1 ))
+
+model.add(AveragePooling2D((1,1)))
+
+# model.add(Conv2D(400,1 ))
+# model.add(AveragePooling2D((1,1)))
 
 
-model.add( Conv2D(256,(3,3)) )
-model.add(Activation("relu") )
-model.add( Conv2D(512,(3,3)) )
-model.add(Activation("relu") )
-model.add(MaxPooling2D(pool_size=(2,2)))
-
-
-model.add( Conv2D(1024,(3,3)) )
-model.add(Activation("relu") )
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(GlobalAveragePooling2D())
 
 
 model.add(Flatten())
-model.add(Dense(2048, activation='relu'))
-model.add(Dropout(0.5))
 
-model.add(Dense(1024, activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dense(200,activation="softmax" ))
+model.summary()
+earlyStopping = EarlyStopping(monitor="val_accuracy" , patience=5 , mode="max")
+mcp_save  = ModelCheckpoint(filepath="best.h5",monitor="val_accuracy",save_best_only=True,mode="max",save_freq="epoch")
+model.compile(Adam(lr=0.0001),loss="sparse_categorical_crossentropy",metrics=["accuracy"])
 
-model.add(Dense(555, activation='softmax'))
+model.fit(x_train,y_train,batch_size=32 ,epochs=200,validation_split=0.1 , callbacks=[mcp_save])
 
-model.compile(loss="sparse_categorical_crossentropy",optimizer="adam",metrics=["accuracy"])
-model.fit(x_train,y_train,batch_size=256 ,epochs=20,validation_split=0.1)
-model.save("test.h5")
-
-# input_layer = tf.keras.Input([200,200,3])
-
-# con1 = tf.keras.layers.Conv2D(filters=32 , kernel_size=(5,5),padding='same',activation="relu")(input_layer)
-
-# pool1 = tf.keras.layers.MaxPooling2D(pool_size=(2,2))(con1)
-
-# con2 = tf.keras.layers(filters = 64 , kernel_size=(3,3) , padding="same",activation="relu")(pool1)
-
-# pool2 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(con2)
-
-# con3 = tf.keras.layers(filters = 96 , kernel_size=(3,3) , padding="same",activation="relu")(pool2)
-
-# pool3 = tf.keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2,2))(con3)
-
-# con4 = tf.keras.layers.Conv2D(filters=96 ,kernel_size=(3,3),padding="same",activation="relu")(pool3)
-
-# pool4 = tf.keras.layers.MaxPooling2D(pool_size  =(2,2),strides=(2,2))(con4)
-
-# fit1 = tf.keras.Flatten()(pool4)
-# dn1 = tf.keras.layers.Dense(512,activation="relu")(fit1)
-# out = tf.keras.layers.Dense(555,activation="softmax")(dn1)
-# model = tf.keras.Model(input_layer,out)
-
-# model.compile(optimizer="adam",loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-# model.fit(x_train,y_train,batch_size = 100,epochs=10)
-# model.save("test.h5")
