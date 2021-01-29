e7190675864a42bc8dfb44e80b72a4f0f9fbf7b5
DreSysm <39560699+DreSysm@users.noreply.github.com>
2021-01-29 14:57:18 +0800

    server.py

--- other/server.py
+++ other/server.py
@@ -2,23 +2,35 @@ from flask import Flask
 from flask_pymongo import PyMongo
 import pymongo
 from pymongo import MongoClient
+import predict
+import requests
 
 
 
-cluster = MongoClient("mongodb+srv://sydres:sydres312154cluster0.8ddot.mongodb.net/<dbname>?retryWrites=true&w=majority")
-db = cluster["bird"]
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client["bird"]
 collection = db["bird_info"]
 
-# app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/return_info') # receive user chosen and return bird info to user.
+def return_info():
+    ans = predict.result("Pied_Kingfisher.jpg")
+    bird_info = collection.find_one({"name":ans[0]})
+
+    return bird_info["description"]
+
+@app.route('/predict_result') #receive photo and predict result ,then give user five options.
+def predict_result():
+
+
+
+    return 0
 
 
 
 
 
 
-# if __name__ == '__main__':
-#    app.run()
+if __name__ == '__main__':
+   app.run(debug=True)
